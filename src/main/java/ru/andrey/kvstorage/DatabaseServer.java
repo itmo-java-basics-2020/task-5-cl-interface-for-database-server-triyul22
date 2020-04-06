b760bc456d5cd78f16ab64fa64f020481c6cbf18
Yulia <trishkinayulia@gmail.com>
2020-04-06 13:52:56 +0300

    add solution

--- src/main/java/ru/andrey/kvstorage/DatabaseServer.java
+++ src/main/java/ru/andrey/kvstorage/DatabaseServer.java
@@ -2,6 +2,11 @@ package ru.andrey.kvstorage;
 
 import ru.andrey.kvstorage.console.DatabaseCommandResult;
 import ru.andrey.kvstorage.console.ExecutionEnvironment;
+import ru.andrey.kvstorage.exception.DatabaseException;
+import ru.andrey.kvstorage.logic.Database;
+import ru.andrey.kvstorage.logic.DatabaseCommands;
+
+import java.util.Optional;
 
 public class DatabaseServer {
 
@@ -12,10 +17,16 @@ public class DatabaseServer {
     }
 
     public static void main(String[] args) {
-
     }
 
     DatabaseCommandResult executeNextCommand(String commandText) {
-        throw new UnsupportedOperationException();
+        if (commandText==null){
+            return  new DatabaseCommandResult.DatabaseCommandResultInnerClass(false, "Command Text is not given");
+        }
+        try {
+            return DatabaseCommands.commandRun(commandText, env);
+        } catch (DatabaseException | IllegalArgumentException e) {
+            return new DatabaseCommandResult.DatabaseCommandResultInnerClass(false, e.getMessage());
+        }
     }
 }
--- src/main/java/ru/andrey/kvstorage/console/DatabaseCommandResult.java
+++ src/main/java/ru/andrey/kvstorage/console/DatabaseCommandResult.java
@@ -3,7 +3,6 @@ package ru.andrey.kvstorage.console;
 import java.util.Optional;
 
 public interface DatabaseCommandResult {
-
     Optional<String> getResult();
 
     DatabaseCommandStatus getStatus();
@@ -15,4 +14,44 @@ public interface DatabaseCommandResult {
     enum DatabaseCommandStatus {
         SUCCESS, FAILED
     }
+
+    class DatabaseCommandResultInnerClass implements DatabaseCommandResult {
+        private boolean success;
+        private String errorMessage;
+
+        public DatabaseCommandResultInnerClass(boolean success, String errorMessage) {
+            this.success = success;
+            this.errorMessage = errorMessage;
+        }
+
+        @Override
+        public Optional<String> getResult() {
+            switch (getStatus()) {
+                case SUCCESS:
+                    return Optional.of("Command successfully executed!");
+                case FAILED:
+                    return Optional.of("Execution of the command has failed." + getErrorMessage());
+            }
+            return Optional.empty();
+        }
+
+        @Override
+        public DatabaseCommandStatus getStatus() {
+            if (isSuccess()) {
+                return DatabaseCommandStatus.SUCCESS;
+            }
+            return DatabaseCommandStatus.FAILED;
+        }
+
+        @Override
+        public boolean isSuccess() {
+            return success;
+        }
+
+        @Override
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
+
 }
